3efa1585137e580e949c4c42ab64385ebb790b4c
pliu25 <pearl.liu25@trinityschoolnyc.org>
2024-06-13 05:10:13 -0400

    update

--- video_filtering.py
+++ video_filtering.py
@@ -2,6 +2,7 @@ import cv2
 import os
 from os import listdir
 import numpy as np 
+import math
 
 def save_frame_range_sec(video_path, start_sec, stop_sec, step_sec,
                          dir_path, basename, ext='jpg'):
@@ -34,7 +35,7 @@ def save_frame_range_sec(video_path, start_sec, stop_sec, step_sec,
             return
         sec += step_sec
 
-save_frame_range_sec('exvideo.mp4',
+save_frame_range_sec('original_video.mp4',
                      0, 11, 1/24,
                      'images', 'sample_video_img')
 
@@ -50,41 +51,150 @@ save_frame_range_sec('exvideo.mp4',
 """
 
 def pixelate(img: list) -> list:
-    for r in range(1, len(img), 2):
+    for r in range(1, len(img), 3):
         row = img[r]
-        for c in range(1, len(row), 2):
- 
+        for c in range(1, len(row), 3):
             blue = 0
             green = 0
             red = 0
-            for pixel_r in range(r-1, r+1):
-                for pixel_c in range(c-1, c+1):
-                    pixel = img[r][c]
-                    blue+=pixel[pixel_r][0]
-                    blue+=pixel[pixel_c][0]
-                    green+=pixel[pixel_r][pixel_c][1]
-                    red+=pixel[pixel_r][pixel_c][2]
-            img[r][c][0] = blue/9 #math.trunc
-            img[r][c][1] = green/9 
-            img[r][c][2] = red/9 
+            pixel = img[r][c]
+            blue+=img[r][c][0]
+            blue+=img[r+1][c][0]
+            blue+=img[r-1][c][0]
+            blue+=img[r][c-1][0]
+            blue+=img[r][c+1][0]
+            blue+=img[r-1][c-1][0]
+            blue+=img[r-1][c+1][0]
+            blue+=img[r+1][c-1][0]
+            blue+=img[r+1][c+1][0]
+            green+=img[r][c][1]
+            green+=img[r+1][c][1]
+            green+=img[r-1][c][1]
+            green+=img[r][c-1][1]
+            green+=img[r][c+1][1]
+            green+=img[r-1][c-1][1]
+            green+=img[r-1][c+1][1]
+            green+=img[r+1][c-1][1]
+            green+=img[r+1][c+1][1]
+            red+=img[r][c][2]
+            red+=img[r+1][c][2]
+            red+=img[r-1][c][2]
+            red+=img[r][c-1][2]
+            red+=img[r][c+1][2]
+            red+=img[r-1][c-1][2]
+            red+=img[r-1][c+1][2]
+            red+=img[r+1][c-1][2]
+            red+=img[r+1][c+1][2]
+            img[r][c][0] = math.trunc(blue/9)
+            img[r+1][c][0] = math.trunc(blue/9)
+            img[r-1][c][0] = math.trunc(blue/9) 
+            img[r][c-1][0] = math.trunc(blue/9) 
+            img[r][c+1][0] = math.trunc(blue/9)
+            img[r-1][c-1][0] = math.trunc(blue/9)
+            img[r-1][c+1][0] = math.trunc(blue/9)
+            img[r+1][c-1][0] = math.trunc(blue/9) 
+            img[r+1][c+1][0] = math.trunc(blue/9)
+            img[r][c][1] = math.trunc(green/9)
+            img[r+1][c][1] = math.trunc(green/9)
+            img[r-1][c][1] = math.trunc(green/9)
+            img[r][c-1][1] = math.trunc(green/9)
+            img[r][c+1][1] = math.trunc(green/9)
+            img[r-1][c-1][1] = math.trunc(green/9)
+            img[r-1][c+1][1] = math.trunc(green/9)
+            img[r+1][c-1][1] = math.trunc(green/9)
+            img[r+1][c+1][1] = math.trunc(green/9)
+            img[r][c][2] = math.trunc(red/9) 
+            img[r+1][c][2] = math.trunc(red/9)
+            img[r-1][c][2] = math.trunc(red/9)
+            img[r][c-1][2] = math.trunc(red/9)
+            img[r][c+1][2] = math.trunc(red/9)
+            img[r-1][c-1][2] = math.trunc(red/9)
+            img[r-1][c+1][2] = math.trunc(red/9)
+            img[r+1][c-1][2] = math.trunc(red/9)
+            img[r+1][c+1][2] = math.trunc(red/9)
+            #print([img[r][c][0], img[r][c][1], img[r][c][2]])
+            #for pixel_r in range(r-1, r+1):
+                #for pixel_c in range(c-1, c+1):
+                    #pixel = img[r][c]
+                    #print("pixel_r", pixel_r)
+                    #print("pixel_c", pixel_c)
+                    #print("pixel", pixel)
+                    #blue+=pixel[pixel_r][0]
+                    #blue+=pixel[pixel_c][0]
+                    #green+=pixel[pixel_r][pixel_c][1]
+                    #red+=pixel[pixel_r][pixel_c][2]
+            #img[r][c][0] = blue/9 #math.trunc
+            #img[r][c][1] = green/9 
+            #img[r][c][2] = red/9 
+    #print("img", img)
     return img
-cv2.imshow(pixelate("/Users/pearl.liu25/Downloads/Video-Filtering-1/images/sample_video_img_000_0.00.jpg"))
+#cv2.imwrite(cv2.imread(f"images/sample_video_img_000_0.00.jpg"), pixelate(cv2.imread(f"images/sample_video_img_000_0.00.jpg")))
+#print(pixelate(cv2.imread(f"images/sample_video_img_000_0.00.jpg")))
+#print((cv2.imread(f"images/sample_video_img_000_0.00.jpg"))[0][0][0])
+def save_image(image, directory, filename):
+    """
+    Save an image to a new directory
+    """
+    cv2.imwrite(os.path.join(directory, filename), image)
 
 
 inPath = "images"
 outPath = "filtered_images"
-"""
+
 for imagePath in os.listdir(inPath):
-    with open(os.path.join(inPath, imagePath)) as f:
-        print(cv2.imread(f"images/{imagePath}"))
-        for imagePath in cv2.imread(f"images/{imagePath}"):
-            #pixelate_rgb(imagePath, 20)
-            print(imagePath)
+    #print("imagePath", pixelate(cv2.imread(f"images/{imagePath}")))
+    mod_image = pixelate(cv2.imread(f"images/{imagePath}"))
+    #os.path.join(outPath, mod_image)
+    # filesplit = os.path.split(imagePath)
+    # file_name = os.path.splitext(filesplit[1])[0]
+    # file_ext = os.path.splitext(imagePath)[1].lower()
+
+    # file_name = file_name + "_modified" + file_ext
+
+    # path = os.path.join(filesplit[0], file_name)
+
+    # cv2.imwrite(path ,src)
+    
+    cv2.imwrite(os.path.join(outPath, 'mod_image' + str(imagePath) + '.jpg'), pixelate(cv2.imread(f"images/{imagePath}")))
+    #cv2.imwrite(os.path.join)
+    #**save_image(pixelate(cv2.imread(f"images/{imagePath}")), outPath, "modified_image.jpg")
+    # with open(os.path.join(inPath, imagePath)) as f:
+    #     print(pixelate(f.read()))
+#         #print(cv2.imread(f"images/{imagePath}"))
+#         for imagePath in cv2.imread(f"images/{imagePath}"):
+#             print(imagePath[0][0][0])
+            #print(pixelate(imagePath))
+            #pixelate(cv2.imread(imagePath))
+#             #print("imagePath", imagePath)
+            # image = pixelate(cv2.imread(imagePath))
+            # filename = 0
+            # save_image(image, outPath, "file" +filename)
+            # filename+=1
+            
+            #if cv2.imread(imagePath) == str:
+                #print("yay")
+            #else:
+                #print("what")
+            #print("imagePath", imagePath)
             #go through every nine pixel boxes 
-"""
+
+mod_images = 'filtered_images'
+video_name = 'filtered_video.mp4'
+
+images = [img for img in os.listdir(mod_images) if img.endswith(".jpg")]
+frame = cv2.imread(os.path.join(mod_images, images[0]))
+height, width, layers = frame.shape
+
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+video = cv2.VideoWriter(video_name, fourcc, 24, (width,height))
+
+for image in images:
+    video.write(cv2.imread(os.path.join(mod_images, image)))
+
+cv2.destroyAllWindows()
+video.release()
 
 
-#https://stackoverflow.com/questions/47143332/how-to-pixelate-a-square-image-to-256-big-pixels-with-python
 
    
 
